4cb65e1be908de23062b16c7f271c097296b18d2
Noe <tecogn88@hotmail.com>
2018-04-16 17:53:38 -0600

    c's de pipas

---
+++ pipas/pipeloop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAXSTRS 5
+
+int main(void){
+	int cntr;
+	FILE *pipe_fp;
+	char *strings[MAXSTRS] = {
+		"echo",
+		"bravo",
+		"alpha",
+		"charlie",
+		"delta"
+	};
+	/* Create one way pipe line with call to popen() */
+	if (( pipe_fp = popen("sort", "w")) == NULL){
+		perror("popen");
+		//exit(1);
+		return 1;
+	}
+	/* Processing loop */
+	for(cntr=0; cntr<MAXSTRS; cntr++) {
+		fputs(strings[cntr], pipe_fp);
+		fputc(1, pipe_fp);
+	}
+	/* Close the pipe */
+	pclose(pipe_fp);
+	return(0);
+}
